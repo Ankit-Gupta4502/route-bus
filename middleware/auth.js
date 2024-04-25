8f664c529db0db2634d7900c1749619f301b947c
israr <israr@hexabells.com>
2024-04-25 14:59:16 +0530

    updated middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,29 @@
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user/user");
 
-const authUser=async(req,res,next)=>{
-    let token=req.header('Authorization')
-try{
-    const user=jwt.verify(token,process.env.JWT)
-    const newUser=await User.findByPk(user.userId);
-    req.user=newUser;
-    next();
-}catch(err){
-    console.log(err)
-    return res.status(401).json('not found user');
-}
-}
+const checkToken = async (req, res, next) => {
+  try {
+    let authToken = req.header["Authorization"] || req.header["authorization"];
+    if (!authToken) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
 
-module.exports=authUser;
+    const token = authToken.replace("Bearer ", "");
+
+    jwt.verify(token, process.env.JWT, (err, decoded) => {
+      if (err) {
+        return res
+          .status(401)
+          .json({ message: "token is not valid", error: err });
+      }
+      req.decoded = decoded;
+      next();
+    });
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json("not found user");
+  }
+};
+
+module.exports = { checkToken };
