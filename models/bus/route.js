8b49b493bfe70ab445ac714ba5843560c95aea7c
israr <israr@hexabells.com>
2024-04-29 15:45:00 +0530

    modify codes

--- models/bus/route.js
+++ models/bus/route.js
@@ -1,3 +1,27 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../../util/database");
+const OwnerDetails = require("../owner/OwnerDetails");
 
+const Route = sequelize.define(
+    "Route",
+    {
+        from: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          to: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+    }
+);
 
+OwnerDetails.hasMany(Route,{
+    foreignKey:"ownerDetailsId"
+  });
+  
+  Route.belongsTo(OwnerDetails);
+  
+  Route.sync();
+  module.exports = Route;
 
