7f8649ea57ec144b8bfb50816e9cd026913396d5
Ankit-Gupta4502 <83642182+Ankit-Gupta4502@users.noreply.github.com>
2024-05-02 17:26:54 +0530

    Update busDetails.js

--- controllers/bus/busDetails.js
+++ controllers/bus/busDetails.js
@@ -1,4 +1,4 @@
-const Bus = require("../../models/bus/busDetails");
+const Bus = require("../../models/bus/BusDetails");
 const conductor = require("../../models/conductor/conductor");
 const Owner = require("../../models/owner/OwnerDetails");
 
